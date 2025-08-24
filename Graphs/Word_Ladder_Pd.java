783f5b82f3d61c94debe212f0130b86313d49d34
Priyadarshan R <Priyadarshan.R@ibm.com>
2025-08-24 11:44:03 +0530

    Solution for Word Ladder by Pd in Java

---
+++ Graphs/Word_Ladder_Pd.java
@@ -0,0 +1,52 @@
+/*
+  Problem Link : https://leetcode.com/problems/word-ladder/
+  Date : 24th August 2025
+ */
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        //convert to set for O(1) lookups
+        Set<String> set = new HashSet<>(wordList);
+
+        //return 0 if endWord is not in the set
+        if(!set.contains(endWord)) return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int distance = 1;
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+            while(size-- > 0 ){
+
+                String currWord = queue.poll();
+                //return distance if endWord is found
+                if(currWord.equals(endWord))return distance;
+
+                char[] charArrayWord = currWord.toCharArray();
+                //try changing each letter
+                for(int i=0;i<charArrayWord.length;i++){
+                    char originalChar = charArrayWord[i];
+
+                    for(char c='a';c<'z';c++){
+                        charArrayWord[i]=c;
+                        String formedString = new String(charArrayWord);
+
+                        //check if the formed string is in the set
+                        if(set.contains(formedString)){
+                            queue.add(formedString);
+                            //remove the formed string from the set
+                            set.remove(formedString);
+                        }
+                    }
+                    //reset the character back to original
+                    charArrayWord[i]=originalChar;
+                }
+            }
+            //increment the distance 
+            distance += 1;
+        }
+        return distance;
+    }
+}
